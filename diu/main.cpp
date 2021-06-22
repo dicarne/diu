c1a3491d58b2d2127ac370563022cc31541a9f8f
dicarne <hzdingjiahe@outlook.com>
2021-06-22 12:31:44 +0800

    clean

--- diu/main.cpp
+++ diu/main.cpp
@@ -6,15 +6,6 @@
 
 int main(int, char **)
 {
-    auto j = json::from("{\"a\":{\"b\":\"20\"}}");
-    auto j2 = make_shared<Object>(ObjectRawType::Struct);
-    auto s = make_shared<Object>("hello");
-    j2->set_child("s", s);
-    j->set_child("c", make_shared<Object>(j2->to_string()));
-    auto js = j->to_string() ;
-    std::cout << js << std::endl;
-    auto jn = json::from(js);
-    return 0;
     vector<string> files;
     files.push_back("test.diu");
     files.push_back("test2.diu");
